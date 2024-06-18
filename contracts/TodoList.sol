903574453ef0a64aa61eaf9ff84a07e2977e53a7
Mugisha Regis <mugisharegis72@gmail.com>
2024-06-18 15:39:09 +0200

    feat:create task and mark as complete

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -23,7 +23,7 @@ contract TodoList {
   );
 
   constructor() public {
-    createTask("Check out dappuniversity.com");
+    createTask("Checkout Dapp Building");
   }
 
   function createTask(string memory _content) public {
